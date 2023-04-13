a3ee24fdf268114afdfd75ca3fa3949f9c4e6332
Varun <37455309+DrnchedNGold@users.noreply.github.com>
2023-04-13 15:21:45 -0400

    Modify program to increase accuracy

--- word_count.c
+++ word_count.c
@@ -5,28 +5,33 @@
 int main (int argc, char** argv) {
 
     int numNewlines = 0, numWords = 0, numBytes = 0;
-    char fileName[41], text[201];
+    char fileName[41] = "input.txt", text[201];
+    int isWhitespace = 1;
 
     //prompt for and read file name
-    puts("Enter file name (40 characters max): ");
-    scanf("%s", fileName);
+    //puts("Enter file name (40 characters max): ");
+    //scanf("%s", fileName);
 
     FILE *inputFile = fopen(fileName, "r");
 
     while (fgets(text, 201, inputFile) != NULL) {   //loop while there is another line in the file and store that line in the text string
         for (int i = 0; text[i] != '\0'; i++) {     //go through each character in the string
-            if(text[i] == ' ' || text[i] == '\t') {     //if character is a whitespace
-                if(text[i - 1] != ' ' && text[i - 1] != '\t') { // and if previous character was not a whitespace then increment words counter
-                    numWords++;     
-                }
-            }
-            if(text[i] == '\n')     //if the character is a new line then increment new line counter
-                numNewlines++;
             numBytes++;         //increment bytes counter
+            if(text[i] == ' ' || text[i] == '\t') {     //if character is a whitespace
+                isWhitespace = 1;
+            }
+            else if(text[i] == '\n') {    //if the character is a new line then increment new line counter
+                numNewlines++;
+                isWhitespace = 1;
+            }
+            else {
+                if(isWhitespace == 1) {
+                    numWords++;
+                }
+                isWhitespace = 0;
+            }
         }
     }
-    numWords++;     //increment words to fix offset
 
-    printf(" %d  %d %d\n", numNewlines, numWords, numBytes);       //print output
-
+    printf("%d  %d %d\n", numNewlines, numWords, numBytes);       //print output
 }
